60103a06ee2f5ceb6a2f51a0584c2d63eb06815a
Dawit Abera <dawitabera64@gmail.com>
2025-02-23 21:12:58 -0800

    fix issues

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,14 +1,21 @@
 resource "aws_vpc" "eks_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = var.vpc_name  # ✅ Use the variable to name the VPC
+    Name = var.vpc_name
   }
 }
 
 resource "aws_subnet" "eks_subnets" {
-  count = 3
-  vpc_id = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.${count.index}.0/24"
+  count = 3  # ✅ Ensure at least two different AZs
+
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = element(["us-west-2a", "us-west-2b", "us-west-2c"], count.index)  # ✅ Different AZs
+
+  tags = {
+    Name = "eks-subnet-${count.index}"
+  }
 }
 
 output "vpc_id" {
@@ -16,5 +23,5 @@ output "vpc_id" {
 }
 
 output "subnet_ids" {
-  value = aws_subnet.eks_subnets[*].id
+  value = aws_subnet.eks_subnets[*].id  # ✅ Ensures EKS gets all subnet IDs
 }
