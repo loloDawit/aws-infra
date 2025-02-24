08ec0252acd51407230eb58574b1441bc3911149
Dawit Abera <dawitabera64@gmail.com>
2025-02-23 20:34:48 -0800

    fix issues

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,5 @@
+variable "repo_name" {
+  description = "ECR Repository Name"
+  type        = string
+  default     = "expense-tracker-repo"
+}
---
+++ modules/eks/variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "eks-cluster"
+}
